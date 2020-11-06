d8e2b2d6a4e57b97fd3b5b6ce2ecb94b96239f21
Antonin Carlin <antonin.carlin@icloud.com>
2020-11-06 22:08:37 +0100

    fix fae version

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export * as _ from "https://deno.land/x/fae/mod.ts";
+export * as _ from "https://deno.land/x/fae@v0.6.2/mod.ts";
 export { emptyDir } from "https://deno.land/std@0.74.0/fs/empty_dir.ts";
 export {
   fetchHttpFile,
